760c22e2074b49baad949772347d7393a1c79a4f
PabloChavez03 <pablooscarchavez@gmail.com>
2023-02-16 14:30:57 -0300

    rm useState in App

--- 01-twitter-card/src/App.jsx
+++ 01-twitter-card/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
 import "./App.css";
 import TwitterFollowCard from "./components/TwitterFollowCard";
 
-
 function App() {
-  return <>
-    <TwitterFollowCard userName={'chavezpablo_'} name="pablito ⭐⭐⭐" initialIsFollowing={true}/>
-    <TwitterFollowCard userName={'chavezpablo_'} name="pablito ⭐⭐⭐"/>
-    <TwitterFollowCard userName={'chavezpablo_'} name="pablito ⭐⭐⭐"/>
-  </>
+  return (
+    <>
+      <TwitterFollowCard
+        userName={"chavezpablo_"}
+        name="pablito ⭐⭐⭐"
+        initialIsFollowing={true}
+      />
+      <TwitterFollowCard userName={"chavezpablo_"} name="pablito ⭐⭐⭐" />
+      <TwitterFollowCard userName={"chavezpablo_"} name="pablito ⭐⭐⭐" />
+    </>
+  );
 }
 
 export default App;
