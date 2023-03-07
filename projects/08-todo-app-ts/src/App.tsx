0d2f2342515c3e35a2afd2a62d28ae1deb16298f
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-07 19:50:43 -0300

    add project 08-todo-app-ts

---
+++ projects/08-todo-app-ts/src/App.tsx
@@ -0,0 +1,21 @@
+const mocks = [{
+  id: '1',
+  title: 'todo 1',
+  completed: false
+}, {
+  id: '1',
+  title: 'todo 1',
+  completed: false
+}, {
+  id: '1',
+  title: 'todo 1',
+  completed: false
+}]
+
+const App = (): JSX.Element => {
+  return (
+    <h1>Todo MVC</h1>
+  )
+}
+
+export default App
