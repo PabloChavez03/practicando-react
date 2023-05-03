7ea4a21e9e93431eb399b615bdb95c1a69d92a09
PabloChavez03 <pablooscarchavez@gmail.com>
2023-05-03 14:42:15 -0300

    add configs

--- projects/10-crud-react-redux/tailwind.config.js
+++ projects/10-crud-react-redux/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // Path to the tremor module
+    "./node_modules/.vite/deps/@tremor_react.js",
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
-		// path tremor node_modules
-		"./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
